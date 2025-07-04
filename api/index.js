49142ef0a48bb9c11339b4ba06d8cfd91b1d02aa
Elite Pro <proelite455@gmail.com>
2025-07-04 21:41:40 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
-let code = require('../pair'); // Update this if needed
+const code = require('./pair'); // moved to same folder for Vercel
 
 app.use('/code', code);
 
 app.use('/pair', (req, res) => {
-  res.sendFile(path.join(__dirname, '../pair.html'));
+  res.sendFile(path.join(__dirname, '../public/pair.html'));
 });
 
 app.use('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../main.html'));
+  res.sendFile(path.join(__dirname, '../public/main.html'));
 });
 
 app.use(bodyParser.json());
